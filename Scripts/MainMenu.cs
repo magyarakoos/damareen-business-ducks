7a34615a2fd244294cef1105da6685b185ae45b6
magyarakoos <magyar.kende.22f@gmail.com>
2025-11-18 11:09:19 +0100

    Create MainMenu.cs

---
+++ Scripts/MainMenu.cs
@@ -0,0 +1,21 @@
+using Godot;
+
+public partial class MainMenu : Control
+{
+	// Member variables here, example:
+	private int _a = 2;
+	private string _b = "textvar";
+
+	public override void _Ready()
+	{
+		// Called every time the node is added to the scene.
+		// Initialization here.
+		GD.Print("Hello from C# to Godot :)");
+	}
+
+	public override void _Process(double delta)
+	{
+		// Called every frame. Delta is time since the last frame.
+		// Update game logic here.
+	}
+}
