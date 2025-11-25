6e1b3df20bca44779c1d7eb11036fb22782ba3b8
magyarakoos <magyar.kende.22f@gmail.com>
2025-11-25 15:08:47 +0100

    Remove empty boilerplate

--- Scripts/MainMenu.cs
+++ Scripts/MainMenu.cs
@@ -1,20 +1,7 @@
 using Godot;
 
 public partial class MainMenu : Control
-{
-	public override void _Ready()
-	{
-		// Called every time the node is added to the scene.
-		// Initialization here.
-		GD.Print("Hello from C# to Godot :)");
-	}
-
-	public override void _Process(double delta)
-	{
-		// Called every frame. Delta is time since the last frame.
-		// Update game logic here.
-	}
-	
+{	
 	private void OnJatekmesterButtonPressed()
 	{
 		GetTree().ChangeSceneToFile("res://Scenes/jatekmester.tscn");
